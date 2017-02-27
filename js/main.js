4adf2e05b56f5f380520805658418a59f03834b7
陳泰澄 <john987john987@gmail.com>
2017-02-27 20:49:48 +0800

    音頻視覺化微調
    音頻視覺化微調
    擴充附檔名圖示

--- js/main.js
+++ js/main.js
@@ -635,11 +635,12 @@ window.onload = function() {
     var canvas = document.getElementsByTagName('canvas')[0],
         cwidth = canvas.width,
         cheight = canvas.height - 2,
-        meterWidth = 10, //width of the meters in the spectrum
-        gap = 2, //gap between meters
+        meterWidth = 6, //width of the meters in the spectrum
+        gap = 1, //gap between meters
         capHeight = 2,
         capStyle = '#fff',
-        meterNum = 800 / (10 + 2), //count of the meters
+        meterNum = 150, //count of the meters
+        // meterNum = 800 / (10 + 2), //count of the meters
         capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
     ctx = canvas.getContext('2d'),
     gradient = ctx.createLinearGradient(0, 0, 0, 300);
@@ -660,13 +661,13 @@ window.onload = function() {
             ctx.fillStyle = capStyle;
             //draw the cap, with transition effect
             if (value < capYPositionArray[i]) {
-                ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
+                ctx.fillRect(i * (meterWidth + gap), cheight - (--capYPositionArray[i]), meterWidth, capHeight);
             } else {
-                ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
+                ctx.fillRect(i * (meterWidth + gap), cheight - value, meterWidth, capHeight);
                 capYPositionArray[i] = value;
             };
             ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
-            ctx.fillRect(i * 12 /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight); //the meter
+            ctx.fillRect(i * (meterWidth + gap) /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight); //the meter
         }
         requestAnimationFrame(renderFrame);
     }
